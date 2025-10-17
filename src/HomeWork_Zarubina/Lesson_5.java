d92a025dd5abd5ba06fc5167972e0c671ce9f59a
ViktoryGold <fenix79797@mail.ru>
2025-10-18 00:13:48 +0300

    Решила очень сложную задачу на нахождение суммы диагонали в уроке № 5

--- src/HomeWork_Zarubina/Lesson_5.java
+++ src/HomeWork_Zarubina/Lesson_5.java
@@ -3,10 +3,13 @@ package HomeWork_Zarubina;
 import java.util.Arrays;
 import java.util.Queue;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Lesson_5 {
     public static void main(String[] args) {
-        fillArray();
+        //fillArray();
+        //getArrayTooth();
+        //getDiagonal();
 
     }
     // Создайте двумерный массив и заполните его псевдослучайными числами
@@ -28,5 +31,56 @@ public class Lesson_5 {
             }
         }
         System.out.println("Максимальное значение: " + max);
+    }
+    // Создать зубчатый n-мерный массив(от 3 уровней).
+    //Посчитать количество ячеек в нём.
+    public static void getArrayTooth(){
+        int[][] ter = new int[5][];
+        ter[0] = new int[4];
+        ter[1] = new int[8];
+        ter[2] = new int[6];
+        ter[3] = new int[3];
+        ter[4] = new int[9];
+        int i = 0;
+        for (int row = 0; row < ter.length; row++){
+            for (int col = 0; col < ter[row].length; col++){
+                i++;
+            }
+        }
+        System.out.println("Создан многомерный зубчатый " + (ter.length) +  "-уровневый массив: \n"
+                + Arrays.deepToString(ter));
+        System.out.println("Количество ячеек = " + i);
+
+
+    }
+    // Найдите сумму элементов на главной диагонали.
+    public static void getDiagonal(){
+        int n = 5;
+        int m = 5;
+        int[][] array = new int[n][m];
+        Random random = new Random();
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                array[i][j] = random.nextInt(100);
+            }
+        }
+        long sum = 0;
+        System.out.println("Получили массив: \n" + Arrays.deepToString(array));
+        System.out.println("Элементы главной диагонали: ");
+        int length = array.length-1;
+        for (int i = length; i >= 0; i--){
+            System.out.print(array[i][i] + " ");
+            System.out.println((sum += array[i][i]));
+        }
+        System.out.println("Сумма элементов главной диагонали = " + sum);
+        System.out.print("\n" +sum);
+        System.out.println("Элементы побочной диагонали: ");
+        for (int i = 0; i <= length; i++){
+            System.out.print(array[i][i] + " ");
+            System.out.println((sum += array[i][i]));
+        }
+        System.out.println("Сумма элементов побочной диагонали = " + sum);
+        System.out.print("\n");
+        System.out.println(Arrays.deepToString(array));
     }
 }
