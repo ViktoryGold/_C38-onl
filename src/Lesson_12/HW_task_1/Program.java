74b8674d2e74171b13dbd1291f059c6ce3c48e1d
VictoryGold <fenix79797@mail.ru>
2025-11-23 00:34:27 +0300

    Решила домашнее задание урока 12. Осталось задание со звёздочкой

--- src/Lesson_12/HW_task_1/Program.java
+++ src/Lesson_12/HW_task_1/Program.java
@@ -1,4 +1,25 @@
 package Lesson_12.HW_task_1;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Program {
+    public static void main(String[] args) {
+        outputAbbreviations();
+
+    }
+    public static void outputAbbreviations() {
+        System.out.println("Введите пожалуйста текст: ");
+        String y = new String(getInput().nextLine());
+        Pattern pattern = Pattern.compile("([А-Я]\\.){2,6}|[А-Я]{2,6}|" +
+                "([A-Z]\\.){2,6}|[A-Z]{2,6}");
+        Matcher matcher = pattern.matcher(y);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+    public static Scanner getInput() {
+        return new Scanner(System.in);
+    }
 }
