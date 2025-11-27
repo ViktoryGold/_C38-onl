2dd0671e6f6373a5dc870c00c72866d83502a12f
VictoryGold <fenix79797@mail.ru>
2025-11-27 11:29:22 +0300

    Решила все практические задания из урока 13

--- src/Lesson_13/Practic_3/Doll.java
+++ src/Lesson_13/Practic_3/Doll.java
@@ -11,4 +11,43 @@ public class Doll {
             System.out.println("Ошибка ввода цены: " + e.getMessage());
         }
     }
+    public void parceDivide(String serialNumber, int divisor) {
+        try {
+            int number = Integer.parseInt(serialNumber);
+            int result = number / divisor;
+            System.out.println("Результат деления: " + result);
+        } catch (NumberFormatException e) {
+            System.out.println("Описание ошибки: " + e.getMessage());
+        } catch (ArithmeticException e) {
+            System.out.println("Описание ошибки: " + e.getMessage());
+        }
+    }
+    public void multipleException(String serialNumber, int accessories) {
+        try {
+            int number = Integer.parseInt(serialNumber);
+            System.out.println("Введён серийный номер: " + number);
+            String[] array = {"платье", "сумочка", "туфли"};
+            System.out.println("Выбранный аксессуар: " + array[accessories]);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Описание ошибки: " + e.getClass().getSimpleName() +
+                    " - " + e.getMessage());
+        }
+    }
+    public void readData(boolean simulateError) {
+        boolean resourceOpened = false;
+        try {
+            resourceOpened = true;
+            System.out.println("Читаем инструкцию...");
+            if (simulateError) {
+                throw new RuntimeException("Произошла ошибка при чтении инструкции");
+            }
+            System.out.println("Инструкция успешно прочитана.");
+        } catch (RuntimeException e) {
+            System.out.println("Ошибка во время чтения: " + e.getMessage());
+        } finally {
+            if (resourceOpened) {
+                System.out.println("Ресурс закрыт.");
+            }
+        }
+    }
 }
--- src/Lesson_13/Practic_3/Program.java
+++ src/Lesson_13/Practic_3/Program.java
@@ -2,8 +2,15 @@ package Lesson_13.Practic_3;
 
 public class Program {
     public static void main(String[] args) {
+        createDoll();
+    }
+    public static void createDoll() {
         Doll barby = new Doll();
+
         barby.getPrice(-9);
+        barby.parceDivide("45", 0);
+        barby.multipleException("12ш", 5);
+        barby.readData(false);
     }
 
 }
