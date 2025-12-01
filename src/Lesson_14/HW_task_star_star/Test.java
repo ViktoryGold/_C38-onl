1e8f2a3713867a7e951dad9e60aea203daf66d9d
VictoryGold <fenix79797@mail.ru>
2025-12-01 10:25:22 +0300

    Решила задание с двумя звёздочками из лекции 14

--- src/Lesson_14/HW_task_star_star/Test.java
+++ src/Lesson_14/HW_task_star_star/Test.java
@@ -1,12 +1,32 @@
 package Lesson_14.HW_task_star_star;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        String line = "contract123n786";
-        if (line.matches("^docnum.{9}") || (line.matches("^contract.{7}"))) {
-            System.out.println("Yes");
+        verification();
+
+    }
+    public static void verification() {
+        try {
+            System.out.println("Please, enter your USERNAME: ");
+            String userName = new Scanner(System.in).nextLine();
+            System.out.println("Please, enter your PASSWORD: ");
+            String password = new Scanner((System.in)).nextLine();
+            initializeException(userName,password);
+        } catch (UserPassException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
+    public static void initializeException(String name, String password) throws UserPassException {
+        String userN = "wers";
+        String pass = "34er5";
+        if (!userN.equals(name)) {
+            throw new UserPassException("Неверный логин");
+        } else if (!pass.equals(password)) {
+            throw new UserPassException("Неверный пароль");
         } else {
-            System.out.println("X");
+            System.out.println("Доступ разрешён!");
         }
     }
 }
---
+++ src/Lesson_14/HW_task_star_star/UserPassException.java
@@ -0,0 +1,7 @@
+package Lesson_14.HW_task_star_star;
+
+public class UserPassException extends Exception {
+    public UserPassException(String message) {
+        super(message);
+    }
+}
